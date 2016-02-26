46609f1f10b1050fc541bfe784a998ca84c0d66b
atomicwest <ogjdne@gmail.com>
2016-02-25 20:27:01 -0800

    zxcv

---
+++ ghostwalker/GhostWalker_beta/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float moveSpeed;
+
+	//include for accessing Animator and .anim sprite sequences
+	private Animator anim; 
+
+	private Rigidbody2D myRigidbody;
+
+	private bool playerMoving;
+
+	//make public for PlayerStartPoint script
+	public Vector2 lastMove;
+
+	private static bool playerExists;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+		myRigidbody = GetComponent<Rigidbody2D> ();
+
+		if (!playerExists) {
+			playerExists = true;
+			DontDestroyOnLoad (transform.gameObject);
+		} else {
+			Destroy (gameObject); 
+		}
+
+		DontDestroyOnLoad (transform.gameObject); //e.g. the player object
+	}
+	
+	// Update is called once per frame
+	//use this section for such things as updating animations for movement
+	void Update () {
+
+		playerMoving = false; 
+
+		//getaxis raw gets the input that is pressed at the moment
+		//two conditions, move to right is "> 0.5f", move to left is "<-0.5f"
+		if (Input.GetAxisRaw ("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f) {
+			//transform.Translate(new Vector3(Input.GetAxisRaw ("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+			//comment out above for rigidbody
+			myRigidbody.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, myRigidbody.velocity.y);
+			playerMoving = true;
+			lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
+		}
+		if (Input.GetAxisRaw ("Vertical") > 0.5f || Input.GetAxisRaw ("Vertical") < -0.5f) {
+			//transform.Translate (new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
+			//comment out above for rigidbody
+			myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, Input.GetAxisRaw("Vertical") * moveSpeed);
+			playerMoving = true;
+			lastMove = new Vector2(0f, Input.GetAxisRaw("Vertical"));
+		}
+
+		//eliminate the sliding effect after bumping off collider boundaries
+		//after commenting out original translate operations
+		if (Input.GetAxisRaw ("Horizontal") < 0.5f && Input.GetAxisRaw ("Horizontal") > -0.5f) {
+			myRigidbody.velocity = new Vector2(0f, myRigidbody.velocity.y);
+		
+		}
+
+		if (Input.GetAxisRaw ("Vertical") < 0.5f && Input.GetAxisRaw ("Vertical") > -0.5f) {
+			myRigidbody.velocity = new Vector2( myRigidbody.velocity.x, 0f);
+			
+		}
+
+		anim.SetFloat ("MoveX", Input.GetAxisRaw ("Horizontal"));
+		anim.SetFloat ("MoveY", Input.GetAxisRaw ("Vertical"));
+		anim.SetBool ("PlayerMoving", playerMoving);
+		anim.SetFloat ("LastMoveX", lastMove.x);
+		anim.SetFloat ("LastMoveY", lastMove.y);
+	}
+
+}
---
+++ ghostwalker/tilemap/RogueKenneyTileset.tsx
@@ -0,0 +1,162 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="BetaTileset" tilewidth="16" tileheight="16" spacing="1" tilecount="1767" columns="57">
+ <image source="../sprite/example/roguelikeSheet_transparent.png" width="968" height="526"/>
+ <terraintypes>
+  <terrain name="brick" tile="236"/>
+  <terrain name="dirt" tile="578"/>
+  <terrain name="blue" tile="744"/>
+ </terraintypes>
+ <tile id="2">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="59">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="61">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="16" width="16"/>
+   <object id="0" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="116">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="16" width="16"/>
+   <object id="0" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="117">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="118">
+  <objectgroup draworder="index">
+   <object id="0" x="16" y="0" height="16"/>
+   <object id="0" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="344">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="16" width="16"/>
+   <object id="0" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="345">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="346">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="401">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="403">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="458">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="459">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="460">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="16" width="16"/>
+   <object id="0" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="520" terrain=",,,1"/>
+ <tile id="521" terrain=",,1,1"/>
+ <tile id="522" terrain=",,1,"/>
+ <tile id="526">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="529">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="577" terrain=",1,,1"/>
+ <tile id="578" terrain="1,1,1,1"/>
+ <tile id="579" terrain="1,,1,"/>
+ <tile id="634" terrain=",1,,"/>
+ <tile id="635" terrain="1,1,,"/>
+ <tile id="636" terrain="1,,,"/>
+ <tile id="686" terrain=",,,2"/>
+ <tile id="687" terrain=",,2,2"/>
+ <tile id="688" terrain=",,2,"/>
+ <tile id="743" terrain=",2,,2"/>
+ <tile id="745" terrain="2,,2,"/>
+ <tile id="800" terrain=",2,,"/>
+ <tile id="801" terrain="2,2,,"/>
+ <tile id="802" terrain="2,,,"/>
+ <tile id="1104">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="16" width="16"/>
+   <object id="0" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="1106">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="1107">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="1108">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="1161">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="1163">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="1164">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="1165">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+</tileset>
